03a8ea7252dcee1713b78e2d715789018e010cca
Michael Molina <l.13molina@gmail.com>
2020-02-06 06:53:01 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,40 @@
-def create_an_empty_array
-  
-end
+def create_an_empty_array 
+  []
+ end
 
-def create_an_array
-  
-end
+def create_an_array 
+  ["I", "am", "really", "learning"] 
+ end
 
-def add_element_to_end_of_array(array, element)
-  
-end
+def add_element_to_end_of_array (array,element)
+  array = ["wow", "I", "am", "really", "learning"]
+  element = add_element_to_end_of_array.push ("arrays!")
+  puts add_element_to_end_of_array
+ end
 
-def add_element_to_start_of_array(array, element)
-  
-end
+def add_element_to_start_of_array(array,element)
+  array = ["I", "am", "really", "learning", "arrays!"]
+  element = add_element_to_start_of_array.unshift ("wow")
+ end
 
 def remove_element_from_end_of_array(array)
-  
-end
+  array = ["wow", "I", "am", "really", "learning", "arrays!"]
+  element = remove_element_from_end_of_array.pop
+ end
 
 def remove_element_from_start_of_array(array)
-  
-end
+  array = ["wow", "I", "am", "really", "learning", "arrays!"]
+  element = remove_element_from_start_of_array.shift
+ end
 
-def retrieve_element_from_index(array, index_number)
-  
-end
+def retrieve_element_from_index(array,index_number)
+  [2] 
+ end
 
 def retrieve_first_element_from_array(array)
-  
-end
+  [0]
+  end
 
 def retrieve_last_element_from_array(array)
-  
+  [-1] 
 end
