19e8cc8f29d8d36ab84fe56a42bd542b318dbdfe
Michael Molina <l.13molina@gmail.com>
2020-02-06 07:46:31 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,30 +7,29 @@ def create_an_array
   end
 
 def add_element_to_end_of_array (array, element)
-  ["wow", "I", "am", "really", "learning"] << "arrays!"
+  (["wow", "I", "am", "really", "learning"] << "arrays!")
   end
 
 def add_element_to_start_of_array(array,element)
-  ["I", "am", "really", "learning", "arrays!"].unshift ("wow")
+  (["I", "am", "really", "learning", "arrays!"].unshift ("wow"))
   end
 
 def remove_element_from_end_of_array(array)
-  ["wow", "I", "am", "really", "learning", "arrays!"].pop
+  (["wow", "I", "am", "really", "learning", "arrays!"].pop)
   end
 
 def remove_element_from_start_of_array(array)
-  ["wow", "I", "am", "really", "learning", "arrays!"].shift
+  (["wow", "I", "am", "really", "learning", "arrays!"].shift)
   end
 
 def retrieve_element_from_index(array,index_number)
-  ["wow", "I", "am", "really", "learning", "arrays!"][2] 
+  (["wow", "I", "am", "really", "learning", "arrays!"][2]) 
   end
 
 def retrieve_first_element_from_array(array)
-  ["wow", "I", "am", "really", "learning", "arrays!"][0]
+  (["wow", "I", "am", "really", "learning", "arrays!"][0])
   end
 
 def retrieve_last_element_from_array(array)
-  ["wow", "I", "am", "really", "learning", "arrays!"][-1] 
-  end
+  (["wow", "I", "am", "really", "learning", "arrays!"][-1]) 
 end
